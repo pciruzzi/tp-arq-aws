f72fcd1d5f4c806e7556285be8b8a5224b64a690
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2018-06-20 17:12:03 -0300

    Hardcode redis version in terraform configuration

--- redis.tf
+++ redis.tf
@@ -4,7 +4,9 @@ resource "aws_elasticache_replication_group" "redis" {
   replication_group_description = "Redis para TP de Arquitecturas"
   node_type                     = "cache.t2.micro"
   number_cache_clusters         = 1
-  parameter_group_name          = "default.redis3.2"
+  engine                        = "redis"
+  engine_version                = "4.0.10"
+  parameter_group_name          = "default.redis4.0"
   port                          = 6379
   security_group_ids            = ["${aws_security_group.apps.id}"]
 
